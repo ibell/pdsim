Updated Broyden method to re-evaluate Jacobian on an as-needed basis Fixed column selection writing to config file
Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

PDSim/core/core.py
@@ -681,6 +681,7 @@ class PDSimCore(object):
         # last index is Itheta, number of steps is Itheta+1
         print 'Itheta steps taken',Itheta+1
         self.Itheta=Itheta
+        self.Ntheta = Itheta
         self.__post_cycle()
         
     def __post_cycle(self):
@@ -793,17 +794,26 @@ class PDSimCore(object):
                 print 'x_state is', x_state
                 
                 if solver_method == 'Euler':
-                    N=self.EulerN
+                    if hasattr(self,'EulerN'):
+                        N=self.EulerN
+                    else:
+                        N=7000
                     self.cycle_SimpleEuler(N,x_state,step_callback=step_callback,
                                            heat_transfer_callback=heat_transfer_callback,
                                            valves_callback=valves_callback)
                 elif solver_method == 'Heun':
-                    N=self.HeunN
+                    if hasattr(self,'HeunN'):
+                        N=self.HeunN
+                    else:
+                        N=7000
                     self.cycle_SimpleEuler(N,x_state,step_callback=step_callback,
                                            heat_transfer_callback=heat_transfer_callback,
                                            valves_callback=valves_callback)
                 elif solver_method == 'RK45':
-                    eps_allowed = self.RK45_eps
+                    if hasattr(self,'RK45_eps'):
+                        eps_allowed=self.RK45_eps
+                    else:
+                        eps_allowed = 1e-8
                     self.cycle_RK45(x_state,
                                     eps_allowed = eps_allowed,
                                     step_callback=step_callback,
@@ -841,13 +851,14 @@ class PDSimCore(object):
                 else:
                     #endcycle_callback returns the errors and new initial st
                     errors, x_state_ = endcycle_callback()
+                    print errors
                     self.x_state = x_state_[:] #Make a copy
                     return errors
                 
             #! End of OBJECTIVE_CYCLE
             
             if UseNR:
-                self.x_state = Broyden(OBJECTIVE_CYCLE, self.x_state, dx = 0.1, itermax = 50, ytol = 1e-4)
+                self.x_state = Broyden(OBJECTIVE_CYCLE, self.x_state, Nfd = 1, dx = 0.01*np.array(self.x_state), itermax = 50, ytol = 1e-4)
                 print self.x_state
                 if self.x_state[0] is None:
                     return None
@@ -898,7 +909,7 @@ class PDSimCore(object):
         
         #end of OJECTIVE
         
-        x_soln = Broyden(OBJECTIVE_ENERGY_BALANCE,[315.0,325.0],dx=0.2,ytol=0.001,itermax=30)
+        x_soln = Broyden(OBJECTIVE_ENERGY_BALANCE,[315.0,325.0],dx=0.3,ytol=0.001,itermax=30)
         print 'Solution is', x_soln,'Td, Tlumps'
         
         del self.resid_Td, self.x_state

PDSim/misc/solvers.py
@@ -35,7 +35,7 @@ def MultiDimNewtRaph(f,x0,dx=1e-6,args=(),ytol=1e-5,w=1.0,JustOneStep=False):
             return x
     return x
         
-def Broyden(f,x0,dx=1e-5,args=(),ytol=1e-5,w=0.75,wJ=0.5,itermax=10,JustOneStep=False):
+def Broyden(f, x0, dx=1e-5, args=(), ytol=1e-5, Nfd = 2, w=1.0, wJ=1.0, itermax=10, JustOneStep=False):
     """
     Broyden's method
     
@@ -47,19 +47,29 @@ def Broyden(f,x0,dx=1e-5,args=(),ytol=1e-5,w=0.75,wJ=0.5,itermax=10,JustOneStep=
     A1=np.zeros((len(x0),len(x0)))
     A0=np.zeros((len(x0),len(x0)))
     
-    #If a float is passed in for dx, convert to a numpy-like list the same shape
-    #as x
     if isinstance(dx,float) or isinstance(dx,int):
         dx=dx*np.ones_like(x0)
-    f0=f(x0,*args)
-    F0=array(f0)
-    if f0 is None:
-        return [None,None]
-    iter=1
-    x1=x0.copy()
-    F1=F0.copy()
+    error_vec=[]
+    
+    def not_improving():
+        # If the error increased in the last step, re-evaluate the Jacobian
+        if len(error_vec)>2 and error_vec[-1]>error_vec[-2]:
+            return True
+        else:
+            return False
+    
+    iter = 1
     while abs(error)>ytol:
-        if iter==1:
+        if iter <= Nfd or not_improving():
+            # If past the first numerical derivative step, use the value of x
+            # calculated below 
+            if iter > 1:
+                x0 = x1.copy()
+            f0=f(x0,*args)
+            F0=array(f0)
+            if f0 is None:
+                return [None,None]
+            
             #Build the Jacobian matrix by columns
             for i in range(len(x0)):
                 epsilon=np.zeros_like(x0)
@@ -70,10 +80,14 @@ def Broyden(f,x0,dx=1e-5,args=(),ytol=1e-5,w=0.75,wJ=0.5,itermax=10,JustOneStep=
                 A0[:,i]=(array(fplus)-F0)/epsilon[i]
             #Get the difference vector
             x1=x0-w*np.dot(inv(A0),F0)
+            print x1,w*np.dot(inv(A0),F0)
             #Just do one step and stop
             if JustOneStep==True:
                 return x1
             iter+=1
+            # Flush the error vector to ensure that you don't continually 
+            # rebuild the Jacobian
+            error_vec=[]
             
         elif iter>1 and iter<itermax:
             #Jacobian updating parameters
@@ -84,7 +98,7 @@ def Broyden(f,x0,dx=1e-5,args=(),ytol=1e-5,w=0.75,wJ=0.5,itermax=10,JustOneStep=
             if f1 is None:
                 return [None,None]
             Y=F1-F0
-            A1=A0+wJ/d*dot((Y-dot(A0,S)),S.T)
+            A1=A0+w/d*dot((Y-dot(A0,S)),S.T)
             x2=x1-w*np.dot(inv(A1),F1)
             #Update values
             x0=x1
@@ -92,6 +106,8 @@ def Broyden(f,x0,dx=1e-5,args=(),ytol=1e-5,w=0.75,wJ=0.5,itermax=10,JustOneStep=
             F0=F1
             A0=A1
             error=np.sqrt(np.sum(np.power(F1,2)))
+            error_vec.append(error)
+            print 'error_vec',error_vec
             iter+=1
         else:
             raise ValueError('Reached maximum number of iterations without getting below ytol RMS error')