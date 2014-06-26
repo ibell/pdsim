826775f41001e2158353acce95e90c0587b0fbe6
Ian Bell <ian.h.bell@gmail.com>
2014-06-26 11:52:06 +0200

    doc building output goes to stdout
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- doc/make.py
+++ doc/make.py
@@ -1,7 +1,7 @@
-import subprocess, os, glob
+import subprocess, os, glob, sys
 
 fp = open('log.txt','w')
-subprocess.call('sphinx-apidoc -e -f -o PDSim_apidoc c:\python27\Lib\site-packages\PDSim c:\python27\Lib\site-packages\PDSim\misc\clipper',shell=True) 
+subprocess.call('sphinx-apidoc -e -f -o PDSim_apidoc c:\python27\Lib\site-packages\PDSim c:\python27\Lib\site-packages\PDSim\misc\clipper',shell=True,stdout = sys.stdout) 
 print 'building docs... logging to log.txt'
 #subprocess.call('make html',stdout = fp,stderr = fp, shell=True)
 subprocess.call('make html',shell=True)
