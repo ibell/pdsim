4aa63065fea5fdb5445d765ef9a31ff4dd27a830
dziviani <davide.ziviani@ugent.be>
2015-05-03 14:51:52 +0200

    added class State_Flooded

--- PDSim/core/StateFlooded.pyx
+++ PDSim/core/StateFlooded.pyx
@@ -781,48 +781,6 @@ cdef class State_Flooded(State):
         def __get__(self):
             return self.get_dudxL()
             
-"""
-#In Cython we would do something equivalent to CoolProp.pxd 
-# This class inheritate from State_Flooded which calculates the mixture properties 
-# and returns the values in a similar way of State class in CoolProp.pxd
 
-cdef class StateFlood:
-    cdef AbstractState pAS
-    cdef readonly bytes Fluid, phase
-    cdef int iFluid,iParam1,iParam2,iOutput
-    cdef int iLiquid    # Maybe ??
-    
-    cdef double T_, rho_, p_, xL_,
-    
-    cpdef set_Fluid(self, string Fluid, string backend)
-    cpdef set_Liquid(self, string Fluid)  #No need for backend
-    
-    #Now the properties are all defined above and we can be consistend with the definition
-    #I used in containers_20150419  @properties
-    
-    cpdef speed_test(self, int N)
-    cpdef update(self, dict params)
-    cpdef update_ph(self, double p, double h)
-    cpdef update_Trho(self, double T, double rho)
-    cpdef State copy(self)
-    cpdef double Props(self, constants_header.parameters iOutput) except *
-    cpdef long Phase(self) except *
-    cpdef double get_Q(self) except *
-    cpdef double get_T(self) except *
-    cpdef double get_p(self) except *
-    cpdef double get_h_m(self) except *
-    cpdef double get_rho_m(self) except *
-    cpdef double get_s_m(self) except *
-    cpdef double get_u_m(self) except *
-    cpdef double get_visc_m(self) except *
-    cpdef double get_cond_m(self) except *
-    cpdef double get_cp_m(self) except *
-    cpdef double get_cp0(self) except *   # TO CHECK
-    cpdef double get_cv_m(self) except *
-    cpdef double get_dpdT(self) except *
-
-
-
-"""
 
 
