c11312deaad7eeef2873a7afbb83f71247512e60
Ian Bell <ian.h.bell@gmail.com>
2015-06-28 18:02:35 -0600

    Update bicubic example to use R410A as a mixture

--- examples/scroll_compressor_bicubic_with_mixtures.py
+++ examples/scroll_compressor_bicubic_with_mixtures.py
@@ -1,4 +1,9 @@
 from scroll_compressor import Compressor
 import CoolProp.CoolProp as CP
-#CP.set_debug_level(10)
-Compressor(Ref= 'BICUBIC&REFPROP::R32[0.6]&R125[0.4]')
+#CP.set_debug_level(1)
+
+# Use this one to use R410A as a mixture
+Compressor(Ref='BICUBIC&REFPROP::R32[0.697614699375863]&R125[0.302385300624138]')
+
+# Use this one to use R410A as a pure fluid
+#Compressor(Ref='R410A')
