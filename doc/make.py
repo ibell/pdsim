2a8abd167a4a84e68c56dacb9b588b7bf72d8bbb
Ian Bell <ian.h.bell@gmail.com>
2015-12-23 20:36:22 -0700

    Updated the docs

--- doc/make.py
+++ doc/make.py
@@ -1,9 +1,11 @@
 import subprocess, os, glob, sys
 
 fp = open('log.txt','w')
-subprocess.call('sphinx-apidoc -e -f -o PDSim_apidoc c:\Miniconda\Lib\site-packages\PDSim',shell=True,stdout = sys.stdout) 
-subprocess.call('sphinx-apidoc -e -f -o GUI_apidoc ..\GUI',shell=True,stdout = sys.stdout) 
+import PDSim
+print os.path.dirname(PDSim.__file__)
+commons = dict(shell=True,stdout = sys.stdout)
+subprocess.call('sphinx-apidoc -e -f -o PDSim_apidoc ' + os.path.dirname(PDSim.__file__), **commons) 
 #~ print 'building docs... logging to log.txt'
 #subprocess.call('make html',stdout = fp,stderr = fp, shell=True)
-subprocess.call('make html',shell=True, stdout=sys.stdout)
-subprocess.call(['make','latex'],stdout = fp,stderr = fp, shell=True)
+subprocess.call(['make','latex'], stdout = fp, stderr = fp)
+subprocess.call('make html', **commons)
