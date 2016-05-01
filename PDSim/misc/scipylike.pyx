4ea8d46bc412727888335b0cc995478b88c833ef
Ian Bell <ian.h.bell@gmail.com>
2016-05-01 16:01:36 -0600

    Fix template specialization of the spline function

--- PDSim/misc/scipylike.pyx
+++ PDSim/misc/scipylike.pyx
@@ -44,7 +44,8 @@ cdef class Spline:
         if number_t is double:
             return self.thisptr.interpolate(x)
         else:
-            return np.array(self.thisptr.interpolate(x))
+            # Select the vector<double> specializatiuon of the interpolation function
+            return np.array(self.thisptr.interpolate[vector[double]](x))
         
 cpdef Spline splrep(vector[double] x, vector[double] y, k = 3, s = 0):
     """
