b58ec48a2a7e2e14b55e7f9f5fcd2704eabac76f
Ian Bell <ian.h.bell@gmail.com>
2017-03-20 19:34:06 -0600

    Tidy up a couple of notebooks

--- doc/notebooks/compile_notebooks.py
+++ doc/notebooks/compile_notebooks.py
@@ -1,14 +1,16 @@
 import subprocess, sys, glob, os
 
+template = '''
+***************************
+Converted IPython notebooks
+***************************
+
+.. note:: These static pages were auto-generated from the IPython notebooks residing `here on github <https://github.com/ibell/pdsim/tree/master/doc/notebooks>`_ They can be also be `viewed with nbviewer <http://nbviewer.ipython.org/github/ibell/pdsim/tree/master/doc/notebooks/>`_
+
+'''
+
 def convert_notebooks():
-    template = '''
-    ***************************
-    Converted IPython notebooks
-    ***************************
-
-    .. note:: These static pages were auto-generated from the IPython notebooks residing `here on github <https://github.com/ibell/pdsim/tree/master/doc/notebooks>`_ They can be also be `viewed with nbviewer <http://nbviewer.ipython.org/github/ibell/pdsim/tree/master/doc/notebooks/>`_
-    '''
-
+    
     thisdir = os.path.abspath(os.path.dirname(__file__))
 
     # Convert the notebooks
@@ -19,14 +21,18 @@ def convert_notebooks():
         fp.write(template)
         fp.write('.. toctree::\n\n')
         for file in glob.glob(thisdir+'/*.rst'):
+            # Read in the converted RST file
             with open(file, 'r') as fin:
                 file_contents= fin.read()
+            # Convert a character
             file_contents = file_contents.replace('%5C','/')
+            # Write back to file
             with open(file, 'w') as fout:
                 fout.write(file_contents)
+            # Write the TOC into the index file again
             if 'index.' in file: continue
             else:
-                fp.write('    ' + file + '\n')
+                fp.write('    ' + os.path.split(file)[1].rsplit('.rst',1)[0] + '\n')
 
 if __name__=='__main__':
     convert_notebooks()
