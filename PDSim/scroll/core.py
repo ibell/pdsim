fc4f79a5c1efff5e7d9b7320565d9591c609e94d
Ian Bell <ian.h.belL@gmail.com>
2017-12-15 21:16:01 -0700

    Fix? the print command

--- PDSim/scroll/core.py
+++ PDSim/scroll/core.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 ##--- package imports
 from PDSim.misc.datatypes import arraym
 from PDSim.core.containers import ControlVolume
