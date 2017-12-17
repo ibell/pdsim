42d6700398d4dfa93d46a1bddb82783c79e9a097
Ian Bell <ian.h.belL@gmail.com>
2017-12-16 23:05:38 -0700

    More absolute_imports

--- PDSim/scroll/scroll_geo.py
+++ PDSim/scroll/scroll_geo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
-from common_scroll_geo import *
-from symm_scroll_geo import *
+from .common_scroll_geo import *
+from .symm_scroll_geo import *
