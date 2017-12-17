64d7e9da355f9be872b1285719bf55871cff300d
Ian Bell <ian.h.belL@gmail.com>
2017-12-16 22:27:02 -0700

    More print_function

--- PDSim/scroll/expander.py
+++ PDSim/scroll/expander.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from math import pi
 import numpy as np
 from PDSim.scroll import scroll_geo
--- PDSim/scroll/plots.py
+++ PDSim/scroll/plots.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 fig_size =  [8,10]
 
