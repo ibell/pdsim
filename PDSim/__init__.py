78f686982777fbdda08e62556dd23ce081787d06
Ian Bell <ian.h.bell@gmail.com>
2018-09-30 17:45:31 +0200

    Remove Coolprop stuff - should pip install

--- PDSim/__init__.py
+++ PDSim/__init__.py
@@ -1,1 +1,3 @@
-__version__ = '2.10'
+__version__ = '2.10.0'
+__git_revision__ = 'aebc60d86e1e210628c0bf12d4404421c4cce093'
+__git_branch__ = 'master'
--- setup_coolprop.py
+++
@@ -1,6 +0,0 @@
-"""
-A convenience script for building CoolProp from the PDSim source directory
-"""
-import subprocess, os, sys
-
-subprocess.check_call(['python','setup.py','build','--force','install'], cwd = os.path.join('externals','coolprop','wrappers','Python'), stdout = sys.stdout)
