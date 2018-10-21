abc04b3f9043370b0c5577e055bcad3d392e1bc2
Ian Bell <ian.h.bell@gmail.com>
2018-10-21 19:17:08 +0200

    Another test for bicubic interpolation

--- examples/scroll_compressor_bicubic_with_mixtures.py
+++ examples/scroll_compressor_bicubic_with_mixtures.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
+
+from PDSim.scroll.core import Scroll
 from scroll_compressor import Compressor
-import CoolProp.CoolProp as CP
-#CP.set_debug_level(1)
+
+# from CoolProp import CoolProp as CP
+# CP.set_config_string(CP.ALTERNATIVE_REFPROP_PATH, r'/home/ian/REFPROP911')
+
+# CP.set_debug_level(1)
 
 # Use this one to use R410A as a mixture
-Compressor(Ref='BICUBIC&REFPROP::R32[0.697614699375863]&R125[0.302385300624138]')
+Compressor(Scroll, Ref='BICUBIC&REFPROP::R32[0.697614699375863]&R125[0.302385300624138]')
 
 # Use this one to use R410A as a pure fluid
 #Compressor(Ref='R410A')
