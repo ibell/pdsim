f93e4a229ff88457bbc9a55a511f3015897517b1
ian <ian.h.bell@gmail.com>
2020-03-30 19:52:53 -0400

    First steps to getting discharge valve working

--- PDSim/scroll/core.py
+++ PDSim/scroll/core.py
@@ -218,6 +218,20 @@ class Scroll(PDSimCore, _Scroll):
                 return 0.0
         except ZeroDivisionError:
             return 0.0
+
+    def DischargeValve(self, FlowPath, **kwargs):
+        if FlowPath.key_down in ['ddd','dd'] or self.discharge_valve.xv[0] == 0:
+            # Pressure in discharge pocket lower than the discharge line
+            # or valve is closed, so no flow
+            return 0.0
+        else:
+            try:
+                FlowPath.A=self.discharge_valve.A()
+                mdot=flow_models.IsentropicNozzle(FlowPath.A,FlowPath.State_up,FlowPath.State_down)
+                print(mdot)
+                return mdot
+            except ZeroDivisionError:
+                return 0.0
             
     def calculate_port_areas(self):
         """ 
