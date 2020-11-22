4ad94d96021278c98fbd98138356ba5d72d42469
ian <ian.h.bell@gmail.com>
2020-11-22 11:52:22 -0500

    Update tests for sums of fx/p fy/p and M_O/p

--- test/test_scroll_geometry.py
+++ test/test_scroll_geometry.py
@@ -1,27 +1,44 @@
 from PDSim.scroll.scroll_geo import set_scroll_geo
-from PDSim.scroll.symm_scroll_geo import SA_forces
-from PDSim.scroll.symm_scroll_geo import setDiscGeo
 from PDSim.scroll.scroll_geo import geoVals
 import PDSim.scroll.symm_scroll_geo as symm
 
 import numpy as np
 
-def check_fxp_fyp_MOp_sums(theta):
+def check_fxp_fyp_MOp_sums(theta, merge_DDD, print_values=False):
     """ 
     All the active chambers conntributing to the forces 
     on the orbiting scroll should sum up to zero because of
     a hydrostatic argument.  If the pressure is the same on 
-    all sides of the scroll, if the forces and and moments
+    all sides of the scroll, if the forces and moments
     do not sum to zero, the scroll would spontaneously fly away
     """
 
     geo = geoVals()
-    set_scroll_geo(100e-6, 2.5, 0.003, 0.005, geo=geo)
-    setDiscGeo(geo, Type='ArcLineArc', r2=0.0008)
+    # set_scroll_geo(100e-6, 2.5, 0.003, 0.005, geo=geo)
+    # setDiscGeo(geo, Type='ArcLineArc', r2=0.0008)
+
+    Vdisp = 0.0001048 #[m^3/rev]
+    Vratio = 2.2 #[-] 
+    t = 0.004 #[m]
+    ro = 0.005 #[m]
+    phi_i0 = 0.0 #[rad]
+    phi_is = 3.141 #[rad]
+    phi_os = 0.3 #[rad]    
+    #  Set the scroll wrap geometry
+    set_scroll_geo(Vdisp, # Vdisp [m^3/rev]
+                   Vratio, # Vratio [-]
+                   t, # Thickness [m]
+                   ro, # Orbiting radius [m]
+                   phi_i0 = phi_i0, # [rad]
+                   phi_os = phi_os, # [rad]
+                   phi_is = phi_is, # [rad]
+                   geo=geo)
+    symm.setDiscGeo(geo, Type='2Arc', r2=0.0)
 
     Nc = symm.getNc(theta, geo)
     fxp, fyp, MOp = 0, 0, 0
-    for chamber in ['SA','S1','S2','C1.1','C1.2','C2.1','C2.2','D1','D2','DD']:
+    disc_chambers = ['DDD'] if merge_DDD else ['D1','D2','DD']
+    for chamber in ['SA','S1','S2','C1.1','C1.2','C2.1','C2.2'] + disc_chambers:
         if chamber[0:2] in ['C1','C2']:
             func = getattr(symm, chamber[0:2] + "_forces")
             alpha = int(chamber.split('.')[1])
@@ -31,18 +48,23 @@ def check_fxp_fyp_MOp_sums(theta):
         else:
             func = getattr(symm, chamber+"_forces")
             o = func(theta, geo)
+        if print_values:
+            print(chamber, o)
         fxp += o['fx_p']
         fyp += o['fy_p']
         MOp += o['M_O_p']
-    assert(abs(fxp) < 1e-16)
-    assert(abs(fyp) < 1e-16)
-    assert(abs(MOp) < 1e-16)
+
+    if not print_values:
+        print(fxp, fyp, MOp)
+        assert(abs(fxp) < 1e-16)
+        assert(abs(fyp) < 1e-16)
+        assert(abs(MOp) < 1e-16)
 
 def test_fxp_fyp_MOpsums():
-    for theta in np.linspace(0, 2*np.pi, 100):
-        yield check_fxp_fyp_MOp_sums, theta
+    for merge_DDD in [True, False]:
+        for theta in np.linspace(0, 2*np.pi, 100):
+            yield check_fxp_fyp_MOp_sums, theta, merge_DDD
 
 if __name__ == '__main__':
-
-    import nose
-    nose.runmodule()
+    # print(check_fxp_fyp_MOp_sums(0.4, False, print_values=True))
+    import nose; nose.runmodule()
