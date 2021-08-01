324a9dae99ae695c51b740ab2adaccfc5fbe5ab0
Ian Bell <ian.h.bell@gmail.com>
2021-08-01 18:14:07 -0400

    Bump to 2.14 with arc-line-arc fix

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ except BaseException as BE:
     print('Error:', BE)
     print('Unable to extract the git branch, set to placeholder')
     
-version = '2.13'
+version = '2.14'
 
 if len(sys.argv) == 1:
     sys.argv += ['clean','develop']
