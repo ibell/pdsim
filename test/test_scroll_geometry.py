ae239840bb994d53d622f9e91d15eedfcabf4624
Ian Bell <ian.h.bell@gmail.com>
2021-08-01 14:44:44 -0400

    Test that all DD geometries yield the same DD spinning moment
    see https://github.com/ibell/pdsim/issues/67

--- test/test_scroll_geometry.py
+++ test/test_scroll_geometry.py
@@ -67,6 +67,44 @@ def test_fxp_fyp_MOp_sums(theta, merge_DDD, disc, print_values=False):
         assert(abs(fyp) < 1e-16)
         assert(abs(MOp) < 1e-16)
 
+
+@pytest.mark.parametrize('theta', np.linspace(0, 2*np.pi, 11))
+@pytest.mark.parametrize('key', ['fx_p','fy_p','M_O_p'])
+def test_DD_forces_always_same(theta, key, print_values=True):
+    """ 
+    All the discharge geometry options must yield the same
+    DD force components by a hydrostatic argument
+    """
+    
+    o = []
+    disc_options = [('2Arc',0.0), ('2Arc', 0.0001), ('ArcLineArc', 0.001)]
+    for i, (disc_type, disc_r2) in enumerate(disc_options):
+        geo = geoVals()
+        Vdisp = 0.0001048 #[m^3/rev]
+        Vratio = 2.2 #[-] 
+        t = 0.004 #[m]
+        ro = 0.005 #[m]
+        phi_i0 = 0.0 #[rad]
+        phi_is = 3.141 #[rad]
+        phi_os = 0.3 #[rad]    
+        #  Set the scroll wrap geometry
+        set_scroll_geo(Vdisp, # Vdisp [m^3/rev]
+                       Vratio, # Vratio [-]
+                       t, # Thickness [m]
+                       ro, # Orbiting radius [m]
+                       phi_i0 = phi_i0, # [rad]
+                       phi_os = phi_os, # [rad]
+                       phi_is = phi_is, # [rad]
+                       geo=geo)
+        symm.setDiscGeo(geo, Type=disc_type, r2=disc_r2)
+        o.append(symm.DD_forces(theta, geo, poly=True)[key])
+    print(o)
+    o = np.array(o)
+    is_ok = all(np.abs(o-np.mean(o)) < 1e-8)
+    assert(is_ok)
+
+    # assert(size(set(o))==1)
+
 if __name__ == '__main__':
     # print(check_fxp_fyp_MOp_sums(0.4, False, print_values=True))
     pytest.main()
