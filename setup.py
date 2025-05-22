ab1b41cae83b943f87acf2b32a865b43b9837460
Ian Bell <ian.h.bell@gmail.com>
2025-05-22 07:24:42 -0400

    Bump to 2.15

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ except BaseException as BE:
     print('Error:', BE)
     print('Unable to extract the git branch, set to placeholder')
     
-version = '2.14'
+version = '2.15'
 
 if len(sys.argv) == 1:
     sys.argv += ['clean','develop']
